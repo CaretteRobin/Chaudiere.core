804860427f8845390f298b07c6a486532abc153e
Robin <robincrtt05@gmail.com>
2025-06-05 14:36:21 +0200

    fix

--- core/application/UseCase/Event/CreateEvent.php
+++ core/application/UseCase/Event/CreateEvent.php
@@ -1,22 +1,37 @@
 <?php
 
-namespace LaChaudiere\core\application\usecase\Event;
-
 use LaChaudiere\core\domain\entities\Event;
+use LaChaudiere\core\domain\entities\Category;
+use LaChaudiere\core\domain\entities\User;
+use LaChaudiere\core\application\exceptions\CategoryNotFoundException;
+use LaChaudiere\core\application\exceptions\UserNotFoundException;
+use Exception;
 
 class CreateEvent
 {
     public function execute(array $data): Event
     {
+        if (!isset($data['title'], $data['category_id'], $data['created_by'])) {
+            throw new Exception("Missing required fields for creating event");
+        }
+
+        if (!Category::find($data['category_id'])) {
+            throw new CategoryNotFoundException($data['category_id']);
+        }
+
+        if (!User::find($data['created_by'])) {
+            throw new UserNotFoundException($data['created_by']);
+        }
+
         return Event::create([
             'title'       => $data['title'],
             'description' => $data['description'] ?? null,
-            'price'       => $data['price'] ?? null,
+            'price'       => $data['price'] ?? 0.00,
             'start_date'  => $data['start_date'] ?? null,
             'end_date'    => $data['end_date'] ?? null,
             'time'        => $data['time'] ?? null,
             'category_id' => $data['category_id'],
-            'created_by'  => $data['created_by']
+            'created_by'  => $data['created_by'],
         ]);
     }
 }
