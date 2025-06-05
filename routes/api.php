b2c0e785d1951c9e56a3a57364ebd6c993aaa1cb
Slophil <slophil.contact@gmail.Com>
2025-06-05 15:32:51 +0200

    refactor: split API and web routes into separate files

---
+++ routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use Psr\Http\Message\ServerRequestInterface;
+use Slim\App;
+use Slim\Http\Interfaces\ResponseInterface;
+use Slim\Routing\RouteCollectorProxy;
+
+return function (App $app) {
+    $app->group('/api', function (RouteCollectorProxy $group) {
+        $group->get('/ping', function (ServerRequestInterface $request, ResponseInterface $response) {
+            $response->getBody()->write(json_encode(['message' => 'pong']));
+            return $response->withHeader('Content-Type', 'application/json');
+        });
+    });
+};
