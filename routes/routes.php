6d809a0188bde645b8d58e8b9a6a93445ad7701b
Robin <robincrtt05@gmail.com>
2025-06-05 15:29:09 +0200

    feat(api): add GET /events route to retrieve all events
    - Register EventService in the DI container
    - Declare GetAllEventsAction route in routes.php
    - Enable JSON response for all events via /events endpoint

--- routes/routes.php
+++ routes/routes.php
@@ -1,12 +1,17 @@
 <?php
 
+declare(strict_types=1);
+
 use Slim\App;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use LaChaudiere\webui\actions\Event\GetAllEventsAction;
 
-return function (App $app) {
-    $app->get('/ping', function (ServerRequestInterface $request, ResponseInterface $response) {
+return function (App $app): void {
+    $app->get('/ping', function (ServerRequestInterface $request, ResponseInterface $response): ResponseInterface {
         $response->getBody()->write(json_encode(['message' => 'pong']));
         return $response->withHeader('Content-Type', 'application/json');
     });
+
+    $app->get('/events', GetAllEventsAction::class);
 };
