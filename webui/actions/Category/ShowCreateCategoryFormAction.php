cc7542480076a88629ffc70474c698220c01fc03
Paul5400 <paul.andrieu54@gmail.com>
2025-06-10 14:55:35 +0200

    feat: add Markdown preview for category description in form

--- webui/actions/Category/ShowCreateCategoryFormAction.php
+++ webui/actions/Category/ShowCreateCategoryFormAction.php
@@ -6,18 +6,25 @@ use LaChaudiere\infra\providers\CsrfTokenProvider;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Slim\Views\Twig;
+use Parsedown;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
 
 class ShowCreateCategoryFormAction
 {
+    /**
+     * @throws SyntaxError
+     * @throws RuntimeError
+     * @throws LoaderError
+     * @throws \Exception
+     */
     public function __invoke(ServerRequestInterface $request, ResponseInterface $response, array $args = []): ResponseInterface
     {
         $view = Twig::fromRequest($request);
 
-        // Generate a CSRF token for the form
         $csrfToken = CsrfTokenProvider::generate();
-
         $user = $request->getAttribute('user');
-
         $params = [];
 
         if ($user) {
@@ -25,6 +32,18 @@ class ShowCreateCategoryFormAction
         }
 
         $params['csrf_token'] = $csrfToken;
+
+        // Récupère les anciennes valeurs du formulaire (si présentes)
+        $old = $request->getParsedBody() ?? [];
+        $params['old'] = $old;
+
+        // Aperçu Markdown -> HTML
+        $descriptionMarkdown = $old['description'] ?? '';
+        $params['descriptionHtml'] = '';
+        if (!empty($descriptionMarkdown)) {
+            $parsedown = new Parsedown();
+            $params['descriptionHtml'] = $parsedown->text($descriptionMarkdown);
+        }
 
         return $view->render($response, 'pages/categories_form.twig', $params);
     }
